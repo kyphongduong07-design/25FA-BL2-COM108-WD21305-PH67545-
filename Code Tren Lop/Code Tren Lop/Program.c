a1aced5c6dc77937e06b3438b40306ace550bcb7
PH67545 <kyphongduong07@gmail.com>
2025-11-29 09:17:43 +0700

    Code Tren Lop

--- Code Tren Lop/Code Tren Lop/Program.c
+++ Code Tren Lop/Code Tren Lop/Program.c
@@ -1,6 +1,91 @@
 #include <stdio.h>
 
-int main()
+void kiemTraSoNguyen()
 {
+	printf("Kiem Tra So Nguyen");
+	printf("\n");
+}
+
+void sapXepPhanTuMang()
+{
+	int mangSoNguyen[100];
+	int length;
+	int tmp;
+	printf("Nhap kich thuoc mang:");
+	scanf("%d", &length);
+	printf("Nhap du lieu cho mang %d phan tu\n", length);
+	//int length = sizeof(mangSoNguyen) / sizeof(int);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d] = ", &i);
+		scanf("%d", &mangSoNguyen[i]);
+	}
+	printf("Xap xep tang dan du lieu mang %d phan tu\n", length);
+	for (int i = 0; i < length - 1; i++)
+	{
+		if (mangSoNguyen[i] > mangSoNguyen[i])
+		{
+			tmp = mangSoNguyen[i];
+			mangSoNguyen[i] = mangSoNguyen[i = 1];
+			mangSoNguyen[i + 1] = tmp;
+			i = -1;
+		}
+	}
+	printf("Xuat du lieu mang %d phan tu\n", length);
+	for (int i = 0; i < length; i++)
+	{
+		printf("mang[%d] = %d\n", i, mangSoNguyen);
+	}
+}
+
+void lapChucNang(int chonChucNang)
+{
+	int tiepTuc = 1;
+	while (tiepTuc == 1)
+	{
+		switch (chonChucNang)
+		{
+		case 1:
+			kiemTraSoNguyen();
+			break;
+		case 2:
+			sapXepPhanTuMang();
+			break;
+		case 3:
+			// ham goi chuc nang 3
+			break;
+		default:
+			printf("Chon sai. Chuc nang hop le [0-3]");
+			break;
+		}
+
+		printf("Tiep tuc thuc hien chuc nang nay? [1=Co | 0=Khong]: ");
+		scanf("%d", &tiepTuc);
+		system("cls");
+	}
 
 }
+
+int main()
+{
+	int chonChucNang;
+	do
+	{
+		printf("Menu");
+		printf("\n");
+		printf("1. Kiem Tra So Nguyen");
+		printf("\n");
+		printf("2. Sap xep phan tu mang");
+		printf("\n");
+		printf("3. TEN chuc nang 3");
+		printf("\n");
+		printf("0. Thoat");
+		printf("\n");
+		printf("Hay chon chuc nang [0-3]: ");
+		scanf("%d", &chonChucNang);
+		lapChucNang(chonChucNang);
+	} while (chonChucNang != 0);
+}
+
+
+// GV: AnhTT184
